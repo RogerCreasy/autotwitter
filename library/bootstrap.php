4e82c93882939e76f28d6b7047eb24ac5a2d9d64
Roger Creasy <Roger.Creasy@gmail.com>
2016-03-02 06:59:38 -0500

    Update bootstrap.php

--- library/bootstrap.php
+++ library/bootstrap.php
@@ -1,5 +1,10 @@
 <?php
-<?php
+
+/*
+* Author: R0ger Creasy
+* Author email: roger.creasy@gmail.com
+*
+*/
 
 require_once (ROOT . DS . 'config' . DS . 'config.php');
 require_once (ROOT . DS . 'library' . DS . 'shared.php');
